fdeefbc6631d1a4daf080132ed5a3b79716b864b
Katarina <radosavljevic.katarina98@gmail.com>
2022-09-14 20:54:03 +0200

    pastry categories table and resource

--- app/Http/Resources/PastryCategoryResource.php
+++ app/Http/Resources/PastryCategoryResource.php
@@ -17,7 +17,7 @@ class PastryCategoryResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'description' => $this->description
+            'ingredients' => $this->ingrediets
         ];
     }
 }
